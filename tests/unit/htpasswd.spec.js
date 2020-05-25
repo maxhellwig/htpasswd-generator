1399a9ef0ba33f57b60a50a1e919a5775feb235b
Max Hellwig <mail@maxhellwig.de>
2020-05-25 14:20:36 +0200

    Remove unit tests

--- tests/unit/htpasswd.spec.js
+++
@@ -1,9 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import Htpasswd from "@/components/Htpasswd.vue";
-
-describe("Htpasswd.vue", () => {
-  it("renders like snapshot", () => {
-    const wrapper = shallowMount(Htpasswd, {});
-    expect(wrapper.element).toMatchSnapshot();
-  });
-});
